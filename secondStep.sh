52f0fa045f4b785fd06514f91a824a30534d00bc
Y Ramesh Rao <yrameshrao@gmail.com>
2020-07-05 15:30:08 +0530

    Temporary changes for the cloning repos

--- secondStep.sh
+++ secondStep.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+git clone https://github.com/Roshani-Light-Towards-Life/litil.git
+git clone https://github.com/Roshani-Light-Towards-Life/nginx_reverse_proxy.git
+git clone https://github.com/Roshani-Light-Towards-Life/area51.git
+
 docker swarm init
-docker stack deploy -c services_swarm.yml root
+# docker stack deploy -c services_swarm.yml root
